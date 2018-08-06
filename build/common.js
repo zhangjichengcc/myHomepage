343c3538ad0bdf261f004b1435201db5060e8b38
zhangjichengcc <zhangjichengcc@163.com>
2018-08-06 16:57:03 +0800

    update

--- build/common.js
+++ build/common.js
@@ -90,7 +90,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "zhangjichengcc.github.io/myHomepage/build/";
+/******/ 	__webpack_require__.p = "/build/";
 /******/ })
 /************************************************************************/
 /******/ ([]);

9559be9633ae3c96712e5c54e1d85e717a02dcdb
zhangjichengcc <zhangjichengcc@163.com>
2018-08-07 15:35:33 +0800

    update

--- build/build.js
+++ build/build.js
@@ -11522,35 +11522,35 @@ webpackJsonp([0,10],[
 	    });
 	};
 	var routes = [{
-	    path: '/',
+	    path: './',
 	    name: 'homePage',
 	    component: home
 	}, {
-	    path: '/login',
+	    path: './login',
 	    name: 'login',
 	    component: login
 	}, {
-	    path: '/about',
+	    path: './about',
 	    name: 'about',
 	    component: about
 	}, {
-	    path: '/article/:id',
+	    path: './article/:id',
 	    name: 'article',
 	    component: article
 	}, {
-	    path: '/userhome/:username',
+	    path: './userhome/:username',
 	    name: 'userhome',
 	    component: userhome
 	}, {
-	    path: '/usermessage',
+	    path: './usermessage',
 	    name: 'usermessage',
 	    component: usermessage
 	}, {
-	    path: '/createtopic',
+	    path: './createtopic',
 	    name: 'createtopic',
 	    component: createtopic
 	}, {
-	    path: '/edittopic/:topicid',
+	    path: './edittopic/:topicid',
 	    name: 'edittopic',
 	    component: edittopic
 	}, {
--- build/common.js
+++ build/common.js
@@ -78,7 +78,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + ".build.js?" + {"0":"c1bad04dfa178fafad0a","1":"19dc396f08cf602b07ef","2":"5e420b43c25e66ffd08d","3":"9e3350dc34fe1ab609fc","4":"0e5df177d75e6776df00","5":"de3ada1dc963f7272d59","6":"e0c2019518078352dd77","7":"9d1b1852aaad886c5288","8":"3c000b47636939c2a3d8","9":"6d5ba3ea89f1d18a6612"}[chunkId] + "";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + ".build.js?" + {"0":"43f64ed6d0bf3b618687","1":"19dc396f08cf602b07ef","2":"5e420b43c25e66ffd08d","3":"9e3350dc34fe1ab609fc","4":"0e5df177d75e6776df00","5":"de3ada1dc963f7272d59","6":"e0c2019518078352dd77","7":"9d1b1852aaad886c5288","8":"3c000b47636939c2a3d8","9":"6d5ba3ea89f1d18a6612"}[chunkId] + "";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
